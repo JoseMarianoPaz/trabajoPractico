e780cec17a4c8460e8246c9d5588adf65b8b3fb7
JoseMarianoPaz <marianopaz1999@gmail.com>
2021-06-15 20:30:41 -0300

    implementando el index

--- app.js
+++ app.js
@@ -3,3 +3,7 @@ const app = express();
 const port = 3030;
 const path = require("path");
 
+app.get("/home",(req,res) => res.sendFile(path.join(__dirname,"views","index.html")));
+
+
+app.listen(port,() =>console.log("servidor corriendo en http://localhost:" + port))
