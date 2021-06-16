0d20b1193894359a7c4ffde86032d77f3dfef8e8
JoseMarianoPaz <marianopaz1999@gmail.com>
2021-06-15 22:25:04 -0300

    agregando las imagenes

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express();
 const port = 3030;
 const path = require("path");
 
+app.use(express.static("public"));
+
 app.get("/home",(req,res) => res.sendFile(path.join(__dirname,"views","index.html")));
 app.get("/babbage",(req,res) => res.sendFile(path.join(__dirname,"views","babbage.html")));
 app.get("/berners-lee",(req,res) => res.sendFile(path.join(__dirname,"views","berners-lee.html")));
